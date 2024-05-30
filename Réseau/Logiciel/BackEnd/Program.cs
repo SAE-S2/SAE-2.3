ebadfacc2a53f268985af3f6291d6fe1e4785bff
Léo <leo.robillard04@gmail.com>
2024-05-30 20:08:58 +0200

    (Edit) : Initialisation Parfaite du Reseau

--- Réseau/Logiciel/BackEnd/Program.cs
+++ Réseau/Logiciel/BackEnd/Program.cs
@@ -7,15 +7,27 @@
         public uint adrOctet2;
         public uint adrOctet3;
         public uint adrOctet4;
+        public uint[] adressebin;
+        public string adrBinaire1;
+        public string adrBinaire2;
+        public string adrBinaire3;
+        public string adrBinaire4;
 
         public int NbOctet = 4;
-        public int NbBit = 32; 
+        public int NbBit = 32;
+        public int NbIP;
+        public int NbMachine;
 
         public uint[] masque;
         public uint msqOctet1;
         public uint msqOctet2;
         public uint msqOctet3;
         public uint msqOctet4;
+        public uint[] masquebin;
+        public string msqBinaire1;
+        public string msqBinaire2;
+        public string msqBinaire3;
+        public string msqBinaire4;
         public uint CIDR = 24;
 
         public char Classe;
@@ -35,6 +47,163 @@
         public uint[] firstIP;
         public uint[] lastIP;
 
+        public void Initialisation()
+        {
+            uint reponse;
+            string rep;
+            Console.WriteLine("Notation binaire du reseau ? [1] : Oui, [2] : Non");
+            reponse = Convert.ToUInt32(Console.ReadLine());
+            switch (reponse)
+            {
+                default:
+                    break;
+                case 1:
+                    for (int i = 1; i <= NbOctet; i++)
+                    {
+                        do
+                        {
+                            Console.WriteLine($"Quel est l'octet {i} de l'adresse réseau ?");
+                            rep = Console.ReadLine();
+                            reponse = Convert.ToUInt32(rep, 2);
+                            if (reponse > 255 || reponse < 0)
+                            {
+                                Console.WriteLine("Erreur : L'octet doit être compris entre 0 et 255");
+                            }
+                        } while (reponse > 255 || reponse < 0);
+                        switch (i)
+                        {
+                            default:
+                                break;
+                            case 1:
+                                adrBinaire1 = rep;
+                                adrOctet1 = reponse;
+                                break;
+                            case 2:
+                                adrBinaire2 = rep;
+                                adrOctet2 = reponse;
+                                break;
+                            case 3:
+                                adrBinaire3 = rep;
+                                adrOctet3 = reponse;
+                                break;
+                            case 4:
+                                adrBinaire4 = rep;
+                                adrOctet4 = reponse;
+                                break;
+                        }
+                    }
+                    break;
+                case 2:
+                    for (int i = 1; i <= NbOctet; i++)
+                    {
+                        do
+                        {
+                            Console.WriteLine($"Quel est l'octet {i} de l'adresse réseau ?");
+                            reponse = Convert.ToUInt32(Console.ReadLine());
+                            rep = Convert.ToString(reponse, 2);
+                            if (reponse > 255 || reponse < 0)
+                            {
+                                Console.WriteLine("Erreur : L'octet doit être compris entre 0 et 255");
+                            }
+                        } while (reponse > 255 || reponse < 0);
+                        switch (i)
+                        {
+                            default:
+                                break;
+                            case 1:
+                                adrOctet1 = reponse;
+                                adrBinaire1 = rep;
+                                break;
+                            case 2:
+                                adrOctet2 = reponse;
+                                adrBinaire2 = rep;
+                                break;
+                            case 3:
+                                adrOctet3 = reponse;
+                                adrBinaire3 = rep;
+                                break;
+                            case 4:
+                                adrOctet4 = reponse;
+                                adrBinaire4 = rep;
+                                break;
+                        }
+                    }
+                    break;
+            }
+            adresse = new uint[NbOctet];
+            adresse[0] = adrOctet1;
+            adresse[1] = adrOctet2;
+            adresse[2] = adrOctet3;
+            adresse[3] = adrOctet4;
+            adressebin = new uint[NbOctet];
+            adressebin[0] = Convert.ToUInt32(adrBinaire1);
+            adressebin[1] = Convert.ToUInt32(adrBinaire2);
+            adressebin[2] = Convert.ToUInt32(adrBinaire3);
+            adressebin[3] = Convert.ToUInt32(adrBinaire4);
+            Console.WriteLine("Notation CIDR du masque ? [1] : Oui, [2] : Non");
+            reponse = Convert.ToUInt32(Console.ReadLine());
+            switch (reponse)
+            {
+                default:
+                    break;
+                case 1:
+                    Console.WriteLine("Quel est le nombre CIDR ?");
+                    CIDR = Convert.ToUInt32(Console.ReadLine());
+                    ConversionCIDR(CIDR);
+                    msqOctet1 = Convert.ToUInt32(msqBinaire1, 2);
+                    msqOctet2 = Convert.ToUInt32(msqBinaire1, 2);
+                    msqOctet3 = Convert.ToUInt32(msqBinaire3, 2);
+                    msqOctet4 = Convert.ToUInt32(msqBinaire4, 2);
+                    break;
+                case 2:
+                    for (int i = 1; i <= NbOctet; i++)
+                    {
+                        do
+                        {
+                            Console.WriteLine($"Quel est l'octet {i} du masque ?");
+                            reponse = Convert.ToUInt32(Console.ReadLine());
+                            rep = Convert.ToString(reponse, 2);
+                            if (reponse > 255 || reponse < 0)
+                            {
+                                Console.WriteLine("Erreur : L'octet doit être compris entre 0 et 255");
+                            }
+                        } while (reponse > 255 || reponse < 0);
+                        switch (i)
+                        {
+                            default:
+                                break;
+                            case 1:
+                                msqOctet1 = reponse;
+                                msqBinaire1 = rep;
+                                break;
+                            case 2:
+                                msqOctet2 = reponse;
+                                msqBinaire2 = rep;
+                                break;
+                            case 3:
+                                msqOctet3 = reponse;
+                                msqBinaire3 = rep;
+                                break;
+                            case 4:
+                                msqOctet4 = reponse;
+                                msqBinaire4 = rep;
+                                break;
+                        }
+                    }
+                    break;
+            }
+            masque = new uint[NbOctet];
+            masque[0] = msqOctet1;
+            masque[1] = msqOctet2;
+            masque[2] = msqOctet3;
+            masque[3] = msqOctet4;
+            masquebin = new uint[NbOctet];
+            masquebin[0] = Convert.ToUInt32(msqBinaire1);
+            masquebin[1] = Convert.ToUInt32(msqBinaire2);
+            masquebin[2] = Convert.ToUInt32(msqBinaire3);
+            masquebin[3] = Convert.ToUInt32(msqBinaire4);
+        }
+
         public void ConversionCIDR(uint CIDR)
         {
             string binaire = string.Empty;
@@ -50,185 +219,89 @@
                 }
             }
             int j = 0;
-            string Octet1 = string.Empty;
-            string Octet2 = string.Empty;
-            string Octet3 = string.Empty;
-            string Octet4 = string.Empty;
+            msqBinaire1 = string.Empty;
+            msqBinaire2 = string.Empty;
+            msqBinaire3 = string.Empty;
+            msqBinaire4 = string.Empty;
             for (int i = j; i < NbBit / 4; i++)
             {
-                Octet1 += binaire[i];
+                msqBinaire1 += binaire[i];
                 j++;
             }
             for (int i = j; i < NbBit / 2; i++)
             {
-                Octet2 += binaire[i];
+                msqBinaire2 += binaire[i];
                 j++;
             }
             for (int i = j; i < NbBit / 2 + NbBit / 4; i++)
             {
-                Octet3 += binaire[i];
+                msqBinaire3 += binaire[i];
                 j++;
             }
             for (int i = j; i < NbBit; i++)
             {
-                Octet4 += binaire[i];
+                msqBinaire4 += binaire[i];
                 j++;
+            }       
+        }
+
+        public void Calculs()
+        {
+            // Détermination Classe
+            if (true)
+            {
+                
             }
-            msqOctet1 = Convert.ToUInt32(Octet1, 2);
-            msqOctet2 = Convert.ToUInt32(Octet2, 2);
-            msqOctet3 = Convert.ToUInt32(Octet3, 2);
-            msqOctet4 = Convert.ToUInt32(Octet4, 2);
-            masque = new uint[NbOctet];
-            masque[0] = msqOctet1;
-            masque[1] = msqOctet2;
-            masque[2] = msqOctet3;
-            masque[3] = msqOctet4;         
+
+            // Calcul adresse net
+
+
+            // Calcul adresse broadcast
+
+
+            // Calcul 1ere IP machine
+
+
+            // Calcul dernière IP machine
+
+
+            // Calcul nombre IPs
+
+
+            // Calcul nombre machines
+
         }
 
         public Reseau()
         {
-            uint reponse;
-            Console.WriteLine("Notation binaire du reseau ? [1] : Oui, [2] : Non");
-            reponse = Convert.ToUInt32(Console.ReadLine());
-            switch (reponse)
-            {
-                default:
-                    break;
-                case 1:
-                    for (int i = 1; i <= NbOctet; i++)
-                    {
-                        do
-                        {
-                            Console.WriteLine($"Quel est l'octet {i} de l'adresse réseau ?");
-                            reponse = Convert.ToUInt32(Console.ReadLine(), 2);
-                            if (reponse > 255 || reponse < 0)
-                            {
-                                Console.WriteLine("Erreur : L'octet doit être compris entre 0 et 255");
-                            }
-                        } while (reponse > 255 || reponse < 0);
-                        switch (i)
-                        {
-                            default:
-                                break;
-                            case 1:
-                                adrOctet1 = reponse;
-                                break;
-                            case 2:
-                                adrOctet2 = reponse;
-                                break;
-                            case 3:
-                                adrOctet3 = reponse;
-                                break;
-                            case 4:
-                                adrOctet4 = reponse;
-                                break;
-                        }
-                    }
-                    adresse = new uint[NbOctet];
-                    adresse[0] = adrOctet1;
-                    adresse[1] = adrOctet2;
-                    adresse[2] = adrOctet3;
-                    adresse[3] = adrOctet4;
-                    break;
-                case 2:
-                    for (int i = 1; i <= NbOctet; i++)
-                    {
-                        do
-                        {
-                            Console.WriteLine($"Quel est l'octet {i} de l'adresse réseau ?");
-                            reponse = Convert.ToUInt32(Console.ReadLine());
-                            if (reponse > 255 || reponse < 0)
-                            {
-                                Console.WriteLine("Erreur : L'octet doit être compris entre 0 et 255");
-                            }
-                        } while (reponse > 255 || reponse < 0);
-                        switch (i)
-                        {
-                            default:
-                                break;
-                            case 1:
-                                adrOctet1 = reponse;
-                                break;
-                            case 2:
-                                adrOctet2 = reponse;
-                                break;
-                            case 3:
-                                adrOctet3 = reponse;
-                                break;
-                            case 4:
-                                adrOctet4 = reponse;
-                                break;
-                        }
-                    }
-                    adresse = new uint[NbOctet];
-                    adresse[0] = adrOctet1;
-                    adresse[1] = adrOctet2;
-                    adresse[2] = adrOctet3;
-                    adresse[3] = adrOctet4;
-                    break;
-            }
-            
-            Console.WriteLine("Notation CIDR du masque ? [1] : Oui, [2] : Non");
-            reponse = Convert.ToUInt32(Console.ReadLine());
-            switch (reponse)
-            {
-                default:
-                    break;
-                case 1:
-                    Console.WriteLine("Quel est le CIDR ?");
-                    CIDR = Convert.ToUInt32(Console.ReadLine());
-                    ConversionCIDR(CIDR);
-                    break;
-                case 2:
-                    for (int i = 1; i <= NbOctet; i++)
-                    {
-                        do
-                        {
-                            Console.WriteLine($"Quel est l'octet {i} du masque ?");
-                            reponse = Convert.ToUInt32(Console.ReadLine());
-                            if (reponse > 255 || reponse < 0)
-                            {
-                                Console.WriteLine("Erreur : L'octet doit être compris entre 0 et 255");
-                            }
-                        } while (reponse > 255 || reponse < 0);
-                        switch (i)
-                        {
-                            default:
-                                break;
-                            case 1:
-                                msqOctet1 = reponse;
-                                break;
-                            case 2:
-                                msqOctet2 = reponse;
-                                break;
-                            case 3:
-                                msqOctet3 = reponse;
-                                break;
-                            case 4:
-                                msqOctet4 = reponse;
-                                break;
-                        }
-                    }
-                    masque = new uint[NbOctet];
-                    masque[0] = msqOctet1;
-                    masque[1] = msqOctet2;
-                    masque[2] = msqOctet3;
-                    masque[3] = msqOctet4;
-                    break;
-            }
-            
+            Initialisation();
+            Calculs();
         }
 
         public void AffichageReseau()
         {
+            Console.Write("Adresse Reseau :");
             for (int i = 0; i < NbOctet; i++)
             {
                 Console.Write($"{adresse[i]} ");
             }
             Console.WriteLine();
+            Console.Write("Adresse Reseau Binaire :");
+            for (int i = 0; i < NbOctet; i++)
+            {
+                Console.Write($"{adressebin[i]} ");
+            }
+            Console.WriteLine();
+            Console.Write("Masque :");
             for (int i = 0; i < NbOctet; i++)
             {
                 Console.Write($"{masque[i]} ");
+            }
+            Console.WriteLine();
+            Console.Write("Masque Binaire :");
+            for (int i = 0; i < NbOctet; i++)
+            {
+                Console.Write($"{masquebin[i]} ");
             }
         }
 
--- Réseau/Logiciel/BackEnd/obj/Debug/net8.0/BackEnd.AssemblyInfo.cs
+++ Réseau/Logiciel/BackEnd/obj/Debug/net8.0/BackEnd.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3871bb4db2d637eb5e2c59914327b4e744f9081")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
